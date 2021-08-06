499e87cb995a0849c59b038ba1dfd71a99819046
Harshey <ee190002045@iiti.ac.in>
2021-08-06 22:46:28 +0530

    image urls

--- board/models.py
+++ board/models.py
@@ -4,17 +4,16 @@ from django.db import models
 class Board(models.Model):
     Title = models.CharField(max_length=35)
     board_id = models.AutoField(primary_key = True, auto_created=True)
-    slug = models.SlugField(default=None)
+    slug = models.SlugField()
 
     def __str__(self) :
         return self.Title
 
 
 class InsideBoard(models.Model) :
-    board_id = models.IntegerField()
+    board_id = models.ForeignKey(Board, default=1, on_delete=models.SET_DEFAULT)
     title_id = models.IntegerField()
     SubTitle = models.CharField(max_length=35)
-    CardTitle = models.CharField(max_length=35)
     CardBody = models.TextField(max_length=500)
 
     def __str__(self):
