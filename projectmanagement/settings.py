7001d4c58f1c0b5c14724a2a6c1ca9cdaa70e958
Harshey <ee190002045@iiti.ac.in>
2021-08-08 22:41:03 +0530

    small change

--- projectmanagement/settings.py
+++ projectmanagement/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    #'django_static_jquery',
+    'django_static_jquery',
     'board'
 ]
 
