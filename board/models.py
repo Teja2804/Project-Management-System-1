377685f8f8900ab7efc451cfc63000a5906e8a57
Harshey <ee190002045@iiti.ac.in>
2021-08-09 00:58:54 +0530

    icon

--- board/models.py
+++ board/models.py
@@ -3,15 +3,15 @@ from django.db import models
 # Create your models here.
 class Board(models.Model):
     Title = models.CharField(max_length=35)
-    board_id = models.AutoField(primary_key = True, auto_created=True)
+    board_id = models.AutoField(primary_key = True)
     slug = models.SlugField()
-
+   
     def __str__(self) :
         return self.Title
 
 
 class InsideBoard(models.Model) :
-    board_id = models.ForeignKey(Board, default=1, on_delete=models.SET_DEFAULT)
+    board_id = models.ForeignKey(Board, default=1, on_delete=models.CASCADE)
     title_id = models.IntegerField()
     SubTitle = models.CharField(max_length=35)
     CardBody = models.TextField(max_length=500)
